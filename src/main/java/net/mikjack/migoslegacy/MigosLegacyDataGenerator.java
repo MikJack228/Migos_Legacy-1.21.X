8d25ff84aa6ec9793fc4dc4427eca3561b9e5593
MikJack228 <galile1.g@yandex.ru>
2024-09-15 18:52:08 +0300

    Initial commit

---
+++ src/main/java/net/mikjack/migoslegacy/MigosLegacyDataGenerator.java
@@ -0,0 +1,11 @@
+package net.mikjack.migoslegacy;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MigosLegacyDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
